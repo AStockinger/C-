11e433d836396164566cb908c05f1952e3fd1550
Amy Stockinger <37304522+AStockinger@users.noreply.github.com>
2018-12-16 19:08:04 -0800

    Add files via upload

---
+++ Labs/Lab5/Menu.hpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+ ** Program name: Lab 5
+ ** Author: Amy Stockinger
+ ** Date: 10/20/18
+ ** Description: Menu function allows user to pick recursive function to use
+******************************************************************************/
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+// allows user to choose which function to call
+void mainMenu();
+
+#endif
---
+++ Labs/Lab5/main.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+ ** Program name: Lab 5
+ ** Author: Amy Stockinger
+ ** Date: 10/20/18
+ ** Description: main function to start menu and allow user to select a
+ ** recursive function.
+******************************************************************************/
+
+#include "Menu.hpp"
+
+int main(){
+
+    mainMenu();
+
+    return 0;
+}
